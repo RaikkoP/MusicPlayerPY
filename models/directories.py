6ea5523c6855b8f8d5b7d73271f4486f80a09c96
Raikko <raikko.prants@gmail.com>
2025-06-29 13:34:29 +0300

    reworking project to include youtube playlist URLS instead of already downloaded music

--- models/directories.py
+++ models/directories.py
@@ -2,21 +2,26 @@ from tkinter import *;
 from tkinter.filedialog import askdirectory
 from typing import List
 from .directory import Directory
+import json
+import os
 
+#TODO: Make it support youtube playslits instead
+#TODO: Save playlist information in a JSON so we can check, if there are already saved ones
 class Directories:
-    current_directories: List[Directory] = []
+    playlists: List[Directory] = []
 
     def  __init__(self):
-        self.current_directories = []
+      self.playlists = []
+      if (os.path.isFile('../data/data.json') == False):
         self.add_new_directory()
 
-    ## Gets a new directory of songs and saves it
+    ## Gets a new directory of songs and saves it"
     def add_new_directory(self):
         path = askdirectory()
         #TODO: TK should open a pop up for the name
         demo = 'demo'
         new_directory = Directory(demo, path)
-        self.current_directories.append(new_directory)
+        self.playlists.append(new_directory)
 
     def log_directories(self):
-        print(f"{self.current_directories[0].path} <----")
+        print(f"{self.playlists[0].path} <----")
